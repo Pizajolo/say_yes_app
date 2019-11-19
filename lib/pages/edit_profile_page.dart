57978e682b515e3a737243177b568e73d5b3740a
simonpiazolo <simonpiazolo@gmail.com>
2019-11-19 19:03:21 +0100

    edited edit profile page added database_service.dart

--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -13,6 +13,33 @@ class EditProfilePage extends StatefulWidget {
 class _EditProfilePageState extends State<EditProfilePage> {
   final _formKey = GlobalKey<FormState>();
 
+  String _name = '';
+  String _bio = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _name = widget.user.username;
+    _bio = widget.user.bio;
+  }
+
+  _submit(){
+    if (_formKey.currentState.validate()){
+      _formKey.currentState.save();
+      // Update user database
+      String _profileImageURL = '';
+      User user = User(
+        id: widget.user.id,
+        username:_name,
+        profileImageUrl: _profileImageURL,
+        bio: _bio,
+      );
+      // Database update
+
+      Navigator.pop(context);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,23 +51,70 @@ class _EditProfilePageState extends State<EditProfilePage> {
       body: SingleChildScrollView(
         child: Container(
           height: MediaQuery.of(context).size.height,
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 60.0,
-                  backgroundImage:
-                      NetworkImage('https://i.redd.it/dmdqlcdpjlwz.jpg'),
-                ),
-                FlatButton(
-                  onPressed: () => print('Change profile image'),
-                  child: Text('Change Profile Image',
-                      style: TextStyle(
-                          color: Theme.of(context).accentColor,
-                          fontSize: 16.0)),
-                )
-              ],
+          child: Padding(
+            padding: const EdgeInsets.all(30.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                children: <Widget>[
+                  CircleAvatar(
+                    radius: 60.0,
+                    backgroundImage:
+                        NetworkImage('https://i.redd.it/dmdqlcdpjlwz.jpg'),
+                  ),
+                  FlatButton(
+                    onPressed: () => print('Change profile image'),
+                    child: Text('Change Profile Image',
+                        style: TextStyle(
+                            color: Theme.of(context).accentColor,
+                            fontSize: 16.0)),
+                  ),
+                  TextFormField(
+                    initialValue: _name,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.person,
+                        size: 30.0,
+                      ),
+                      labelText: 'Name',
+                    ),
+                    validator: (input) => input.trim().length < 1
+                        ? 'Please enter a valid name'
+                        : null,
+                    onSaved: (input) => _name = input,
+                  ),
+                  TextFormField(
+                    initialValue: _bio,
+                    style: TextStyle(fontSize: 18.0),
+                    decoration: InputDecoration(
+                      icon: Icon(
+                        Icons.book,
+                        size: 30.0,
+                      ),
+                      labelText: 'Bio',
+                    ),
+                    validator: (input) => input.trim().length > 150
+                        ? 'Please enter a bio less than 150 characters'
+                        : null,
+                    onSaved: (input) => _bio = input,
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(40.0),
+                    height: 40.0,
+                    width: 250.0,
+                    child: FlatButton(
+                      onPressed: _submit,
+                      color: Colors.blueAccent,
+                      textColor: Colors.white,
+                      child: Text(
+                        'Save Profile',
+                        style: TextStyle(fontSize: 18.0),
+                      ),
+                    )
+                  )
+                ],
+              ),
             ),
           ),
         ),
